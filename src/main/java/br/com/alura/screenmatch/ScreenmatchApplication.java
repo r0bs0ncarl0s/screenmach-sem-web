9d1f67e8805b5a18209c24934f2e95ee32867e85
r0bs0ncarl0s <r0bs0ncarl0s@hotmail.com>
2024-03-21 11:48:53 -0300

    Parametrizando o CEP

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,20 +1,10 @@
 package br.com.alura.screenmatch;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.alura.screenmatch.model.DadosCEP;
-import br.com.alura.screenmatch.model.DadosEpisodio;
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.principal.Principal;
-import br.com.alura.screenmatch.service.ConsumoAPI;
-import br.com.alura.screenmatch.service.ConverteDados;
-import br.com.alura.screenmatch.utils.Constantes;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner{
@@ -25,20 +15,8 @@ public class ScreenmatchApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
 		Principal principal = new Principal(); 
-		principal.buscarSeriePor("ID");
-		
-		//Buscar CEP
-		ConsumoAPI consumoAPI = new ConsumoAPI(); 
-		ConverteDados conversor = new ConverteDados();
-		List<String> listaCEP = new ArrayList<>();
-		listaCEP.add("26255155");
-		listaCEP.add("26013310");
-		for (String itemCEP : listaCEP) {
-			var json = consumoAPI.obterDados(Constantes.HTTPS + Constantes.API_CEP + itemCEP + Constantes.API_CEP_COMPLEMENTO_FINAL);
-			DadosCEP dadosCEP = conversor.obterDados(json, DadosCEP.class);
-			System.out.println(dadosCEP);
-		}
+		principal.buscarSeriePor("nome");
+		principal.buscarCEP();
 	}
 }
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
+import br.com.alura.screenmatch.model.DadosCEP;
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
@@ -20,40 +21,58 @@ public class Principal {
 	private ConsumoAPI consumo 			= new ConsumoAPI();
 	private ConverteDados conversor 	= new ConverteDados();
 	
-	public void buscarSeriePor(String tipo) throws JsonMappingException, JsonProcessingException {
-		if(tipo!=null && !tipo.equals("")) {
-			String auxBusca = "";
-			String aux = "";
-			if(tipo.trim().equalsIgnoreCase("nome")) {
-				aux = "nome";
-			}else if(tipo.trim().equalsIgnoreCase("id")) {
-				aux = "ID";
-			}
-			System.out.println("Digite o " + aux + " da série para burca:");
-			auxBusca = leitura.nextLine();			
-			var json = consumo.obterDados(Constantes.HTTPS + Constantes.API_FILMES_SERIES + auxBusca.replace(" ", "+") + 
-										  Constantes.API_KEY + Constantes.OMDBAPI_KEY);
-			if(json.contains("Movie not found!")) {
-				System.out.println("Filme/Série não encontrada");
-			}else {
-				DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-				System.out.println(dados);
-				json = consumo.obterDados(Constantes.HTTPS + Constantes.API_FILMES_SERIES + auxBusca.replace(" ", "+") + 
-										  Constantes.SEASON + "1" + 
-										  Constantes.EPISODE + "2" + 
-										  Constantes.API_KEY + Constantes.OMDBAPI_KEY);
-				DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-				System.out.println(dadosEpisodio);				
-				List<DadosTemporada> listDadosTemporada = new ArrayList<>(); 
-				for (int i = 1; i < dados.totalTemporadas(); i++) {
-					json = consumo.obterDados(Constantes.HTTPS + Constantes.API_FILMES_SERIES + auxBusca.replace(" ", "+") + 
-												 Constantes.SEASON  + i + 
-												 Constantes.API_KEY + Constantes.OMDBAPI_KEY);
-					DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-					listDadosTemporada.add(dadosTemporada);
+	public void buscarSeriePor(String tipo) {
+		try {
+			if(tipo!=null && !tipo.equals("")) {
+				String auxBusca = "";
+				String aux = "";
+				if(tipo.trim().equalsIgnoreCase("nome")) {
+					aux = "nome";
+				}else if(tipo.trim().equalsIgnoreCase("id")) {
+					aux = "ID";
 				}
-				listDadosTemporada.forEach(System.out::println);
+				System.out.println("Digite o " + aux + " da série para burca:");
+				auxBusca = leitura.nextLine();			
+				var json = consumo.obterDados(Constantes.HTTPS + Constantes.API_FILMES_SERIES + auxBusca.replace(" ", "+") + 
+											  Constantes.API_KEY + Constantes.OMDBAPI_KEY);
+				if(json.contains("Movie not found!")) {
+					System.out.println("Filme/Série não encontrada");
+				}else {
+					DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+					System.out.println(dados);
+					json = consumo.obterDados(Constantes.HTTPS + Constantes.API_FILMES_SERIES + auxBusca.replace(" ", "+") + 
+											  Constantes.SEASON + "1" + 
+											  Constantes.EPISODE + "2" + 
+											  Constantes.API_KEY + Constantes.OMDBAPI_KEY);
+					DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+					System.out.println(dadosEpisodio);				
+					List<DadosTemporada> listDadosTemporada = new ArrayList<>(); 
+					for (int i = 1; i < dados.totalTemporadas(); i++) {
+						json = consumo.obterDados(Constantes.HTTPS + Constantes.API_FILMES_SERIES + auxBusca.replace(" ", "+") + 
+													 Constantes.SEASON  + i + 
+													 Constantes.API_KEY + Constantes.OMDBAPI_KEY);
+						DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+						listDadosTemporada.add(dadosTemporada);
+					}
+					listDadosTemporada.forEach(System.out::println);
+				}
 			}
-		}
+		}catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}	
+	}
+	
+	public void buscarCEP(){
+		try {
+			System.out.println("Digite o CEP desejado:");
+			String auxBusca = leitura.nextLine();
+			var json = consumo.obterDados(Constantes.HTTPS + Constantes.API_CEP + auxBusca + Constantes.API_CEP_COMPLEMENTO_FINAL);
+			DadosCEP dadosCEP = conversor.obterDados(json, DadosCEP.class);
+			System.out.println(dadosCEP);
+		}catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}		
 	}
 }
