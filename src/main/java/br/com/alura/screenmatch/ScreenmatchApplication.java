9bf08209ac05d92f1e421f16525665f25bb90333
r0bs0ncarl0s <r0bs0ncarl0s@hotmail.com>
2024-03-21 11:41:05 -0300

    Segunda melhoria

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -11,6 +11,7 @@ import br.com.alura.screenmatch.model.DadosCEP;
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.principal.Principal;
 import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverteDados;
 import br.com.alura.screenmatch.utils.Constantes;
@@ -24,36 +25,18 @@ public class ScreenmatchApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		ConsumoAPI consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados(Constantes.HTTPS + Constantes.API_FILMES_SERIES + "gilmore+girls" + 
-										 Constantes.API_KEY + Constantes.OMDBAPI_KEY);
-		//System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-		json = consumoAPI.obterDados(Constantes.HTTPS + Constantes.API_FILMES_SERIES + "gilmore+girls" + 
-									 Constantes.SEASON + "1" + 
-									 Constantes.EPISODE + "2" + 
-									 Constantes.API_KEY + Constantes.OMDBAPI_KEY);
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
 		
-		List<DadosTemporada> listDadosTemporada = new ArrayList<>(); 
-		for (int i = 1; i < dados.totalTemporadas(); i++) {
-			json = consumoAPI.obterDados(Constantes.HTTPS + Constantes.API_FILMES_SERIES + "gilmore+girls" + 
-										 Constantes.SEASON  + i + 
-										 Constantes.API_KEY + Constantes.OMDBAPI_KEY);
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			listDadosTemporada.add(dadosTemporada);
-		}
-		listDadosTemporada.forEach(System.out::println);
+		Principal principal = new Principal(); 
+		principal.buscarSeriePor("ID");
 		
 		//Buscar CEP
+		ConsumoAPI consumoAPI = new ConsumoAPI(); 
+		ConverteDados conversor = new ConverteDados();
 		List<String> listaCEP = new ArrayList<>();
 		listaCEP.add("26255155");
 		listaCEP.add("26013310");
 		for (String itemCEP : listaCEP) {
-			json = consumoAPI.obterDados(Constantes.HTTPS + Constantes.API_CEP + itemCEP + Constantes.API_CEP_COMPLEMENTO_FINAL);
+			var json = consumoAPI.obterDados(Constantes.HTTPS + Constantes.API_CEP + itemCEP + Constantes.API_CEP_COMPLEMENTO_FINAL);
 			DadosCEP dadosCEP = conversor.obterDados(json, DadosCEP.class);
 			System.out.println(dadosCEP);
 		}
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,18 +1,59 @@
 package br.com.alura.screenmatch.principal;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+import br.com.alura.screenmatch.model.DadosEpisodio;
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.utils.Constantes;
 
 public class Principal {
 	
-	private Scanner leitura = new Scanner(System.in);
-	private final String ENDERECO = "https://www.omdbapi.com/?t=";
-	private final String API_KEY = "&apikey=6585022c";
-	private ConsumoAPI consumo = new ConsumoAPI();  
-	public void exibeMenu() {
-		System.out.println("Digite o nome da série");
-		var nomeSerie = leitura.nextLine();
-		var json = consumo.obterDados(ENDERECO + nomeSerie.replace("", "+") + API_KEY);		
+	private Scanner leitura 			= new Scanner(System.in);
+	private ConsumoAPI consumo 			= new ConsumoAPI();
+	private ConverteDados conversor 	= new ConverteDados();
+	
+	public void buscarSeriePor(String tipo) throws JsonMappingException, JsonProcessingException {
+		if(tipo!=null && !tipo.equals("")) {
+			String auxBusca = "";
+			String aux = "";
+			if(tipo.trim().equalsIgnoreCase("nome")) {
+				aux = "nome";
+			}else if(tipo.trim().equalsIgnoreCase("id")) {
+				aux = "ID";
+			}
+			System.out.println("Digite o " + aux + " da série para burca:");
+			auxBusca = leitura.nextLine();			
+			var json = consumo.obterDados(Constantes.HTTPS + Constantes.API_FILMES_SERIES + auxBusca.replace(" ", "+") + 
+										  Constantes.API_KEY + Constantes.OMDBAPI_KEY);
+			if(json.contains("Movie not found!")) {
+				System.out.println("Filme/Série não encontrada");
+			}else {
+				DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+				System.out.println(dados);
+				json = consumo.obterDados(Constantes.HTTPS + Constantes.API_FILMES_SERIES + auxBusca.replace(" ", "+") + 
+										  Constantes.SEASON + "1" + 
+										  Constantes.EPISODE + "2" + 
+										  Constantes.API_KEY + Constantes.OMDBAPI_KEY);
+				DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+				System.out.println(dadosEpisodio);				
+				List<DadosTemporada> listDadosTemporada = new ArrayList<>(); 
+				for (int i = 1; i < dados.totalTemporadas(); i++) {
+					json = consumo.obterDados(Constantes.HTTPS + Constantes.API_FILMES_SERIES + auxBusca.replace(" ", "+") + 
+												 Constantes.SEASON  + i + 
+												 Constantes.API_KEY + Constantes.OMDBAPI_KEY);
+					DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+					listDadosTemporada.add(dadosTemporada);
+				}
+				listDadosTemporada.forEach(System.out::println);
+			}
+		}
 	}
 }
